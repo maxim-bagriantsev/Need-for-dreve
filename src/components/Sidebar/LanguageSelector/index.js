292f3cc941f4582248f2fd7467eaa05be5e11d6f
maxim-bagriantsev <maxim163rus@gmail.com>
2021-08-27 13:05:03 +0400

    added toggle languages

--- src/components/Sidebar/LanguageSelector/index.js
+++ src/components/Sidebar/LanguageSelector/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './languageSelector.scss';
 
 export const LanguageSelector = () => {
+    const [language, setLanguage] = useState('Eng')
+
+    const hendleChangeLanguage = () => {
+        if (language === 'Eng') {
+            setLanguage('Рус')
+        } else {
+            setLanguage('Eng')
+        }
+    }
 
     return (
         <div className='languageSelector'>
-            <span className="without-class">Eng</span>
+            <span className="without-class" onClick={hendleChangeLanguage}>{language}</span>
         </div>
     )
 }
+
+
