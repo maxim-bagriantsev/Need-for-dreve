bdbb66d9d77fc2f9b31fd202f61f16e7bb942d3a
maxim-bagriantsev <maxim163rus@gmail.com>
2021-09-01 23:18:22 +0400

    F-01 refactoring component Carousel

--- src/components/Slider/Carousel/Carousel.js
+++ src/components/Slider/Carousel/Carousel.js
@@ -1,22 +1,78 @@
-import React, {useRef, useState} from "react";
+import React from "react";
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import "./carousel.scss";
 
-import free_parking from '../../../assets/imeges/free-parking.png'
-import insurance from '../../../assets/imeges/insurance.png'
-import gasoline from '../../../assets/imeges/gasoline.png'
-import service from '../../../assets/imeges/service.png'
-
+//Импорт стандартных компонентов из подключенной библиотеки swiper
 import SwiperCore, {
-    Navigation, Pagination, Mousewheel, Keyboard
+    Navigation,
+    Pagination,
+    Mousewheel,
+    Keyboard
 } from 'swiper';
 
-SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
-export const Carousel = () => {
+//Картинки к слайдеру
+import free_parking from "../../../assets/imeges/free-parking.png";
+import insurance from "../../../assets/imeges/insurance.png";
+import gasoline from "../../../assets/imeges/gasoline.png";
+import service from "../../../assets/imeges/service.png";
 
+SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
+
+const slides = [
+    {
+        id: 1,
+        img: free_parking,
+        title: 'Бесплатная парковка',
+        text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.',
+        classNameButton: 'slider-btn-1'
+    },
+    {
+        id: 2,
+        img: insurance,
+        title: 'Страховка',
+        text: 'Полная страховка страховка автомобиля',
+        classNameButton: 'slider-btn-2'
+    },
+    {
+        id: 3,
+        img: gasoline,
+        title: 'Бензин',
+        text: 'Полный бак на любой заправке города за наш счёт.',
+        classNameButton: 'slider-btn-3'
+    },
+    {
+        id: 4,
+        img: service,
+        title: 'Обслуживание',
+        text: 'Автомобиль проходит еженедельное ТО',
+        classNameButton: 'slider-btn-4'
+    }
+]
+
+export const Carousel = () => {
+    const elementSlide = slides.map((slide, id) => {
+
+        return (
+            <SwiperSlide key={id}>
+                <div className='slider' style={{backgroundImage: `url(${slide.img})`}}>
+                    <div className='slider-content'>
+                        <div className='slider-content-item'>
+                            <div className='swiper-button-prev'/>
+                            <h2>{slide.title}</h2>
+                            <h3>{slide.text}</h3>
+                            <button className={slide.classNameButton}>
+                                <span>Подробнее</span>
+                            </button>
+                            <div className='swiper-button-next'/>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+        )
+    })
     return (
         <>
             <Swiper
@@ -28,68 +84,7 @@ export const Carousel = () => {
                 loop={true}
                 slidesPerview={0}
                 className="slider">
-                <SwiperSlide>
-                    <div className='slider' style={{backgroundImage: `url(${free_parking})`}}>
-                        <button className='btn-arrow'/>
-                        <div className='slider-content'>
-                            <div className='slider-content-item'>
-                                <h2>Бесплатная парковка</h2>
-                                <h3>Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая
-                                    ПДД, а также в
-                                    аэропортах.</h3>
-                                <button className='slider-btn-1'>
-                                    <span>Подробнее</span>
-                                </button>
-                            </div>
-                        </div>
-                        <button className='btn-arrow'/>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='slider' style={{backgroundImage: `url(${insurance})`}}>
-                        <button className='btn-arrow'/>
-                        <div className='slider-content'>
-                            <div className='slider-content-item'>
-                                <h2>Страховка</h2>
-                                <h3>Полная страховка автомобиля</h3>
-                                <button className='slider-btn-2'>
-                                    <span>Подробнее</span>
-                                </button>
-                            </div>
-                        </div>
-                        <button className='btn-arrow'/>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='slider' style={{backgroundImage: `url(${gasoline})`}}>
-                        <button className='btn-arrow'/>
-                        <div className='slider-content'>
-                            <div className='slider-content-item'>
-                                <h2>Бензин</h2>
-                                <h3>Полный бак на любой заправке города за наш счёт</h3>
-                                <button className='slider-btn-3'>
-                                    <span>Подробнее</span>
-                                </button>
-                            </div>
-                        </div>
-                        <button className='btn-arrow'/>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='slider' style={{backgroundImage: `url(${service})`}}>
-                        <button className='btn-arrow'/>
-                        <div className='slider-content'>
-                            <div className='slider-content-item'>
-                                <h2>Обслуживание</h2>
-                                <h3>Автомобиль проходит еженедельное ТО</h3>
-                                <button className='slider-btn-4'>
-                                    <span>Подробнее</span>
-                                </button>
-                            </div>
-                        </div>
-                        <button className='btn-arrow'/>
-                    </div>
-                </SwiperSlide>
+                {elementSlide}
             </Swiper>
         </>
     )
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,69 +1,10 @@
 import React from 'react';
 import './slider.scss';
-import free_parking from '../../assets/imeges/free-parking.png'
-import insurance from '../../assets/imeges/insurance.png'
-import gasoline from '../../assets/imeges/gasoline.png'
-import service from '../../assets/imeges/service.png'
+
 import {Carousel} from "./Carousel/Carousel";
 
-
-const slides = [
-    {
-        img1: free_parking,
-        title: 'Бесплатная парковка',
-        text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.'
-    },
-    {
-        img2: insurance,
-        title: 'Бесплатная парковка',
-        text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.'
-    },
-    {
-        img3: gasoline,
-        title: 'Бесплатная парковка',
-        text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.'
-    },
-    {
-        img4: service,
-        title: 'Бесплатная парковка',
-        text: 'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.'
-    }
-]
-
-
 export const Slider = () => {
-
-    return <Carousel slides={slides}/>
-
+    return <Carousel/>
 }
 
 
-/*
-<div className='slider'>
-    <button className='btn-arrow'>
-        <svg className='btn-arrow-icon' width='10' height='20'>
-            <use xlinkHref={`${icons}#Arrow-Left`}/>
-        </svg>
-    </button>
-    <div className='slider-content'>
-        <div className='slider-content-item'>
-            <h2>Бесплатная парковка</h2>
-            <h3>Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в
-                аэропортах.</h3>
-            <button className='slider-btn-1'>
-                <span>Подробнее</span>
-            </button>
-        </div>
-        <div className='slider-dots'>
-            <div className='slider-dot-active'/>
-            <div className='slider-dot'/>
-            <div className='slider-dot'/>
-            <div className='slider-dot'/>
-        </div>
-    </div>
-    <button className='btn-arrow'>
-        <svg className='btn-arrow-icon' width='10' height='20'>
-            <use xlinkHref={`${icons}#Arrow-Right`}/>
-        </svg>
-    </button>
-</div>*/
