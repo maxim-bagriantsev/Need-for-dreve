fc8e119454150656b0a763ba5e34c8ae9cc3fb58
maxim-bagriantsev <maxim163rus@gmail.com>
2021-09-18 22:45:11 +0400

    V-06 added orderFinish page

--- src/components/OrderContent/InfoOrderButton/index.js
+++ src/components/OrderContent/InfoOrderButton/index.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import {Modal} from 'antd';
-import {NavLink} from "react-router-dom";
 import './button.scss';
 import 'antd/dist/antd.css';
 import './index.css';
